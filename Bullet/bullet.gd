3246c3c7d59f1b57bd654b43b5b954aec391af32
SolOloughlin <sol_oloughlin@raglanarea.school.nz>
2025-07-15 10:38:41 +1200

    15/07

--- Bullet/bullet.gd
+++ Bullet/bullet.gd
@@ -1,7 +1,7 @@
 extends AnimatedSprite2D
 
 @export var speed: int = 600
-@export var direction: int
+@export var direction: float
 
 func _physics_process(delta: float) -> void:
 	move_local_x(direction * speed * delta)
--- player/player.gd
+++ player/player.gd
@@ -34,9 +34,7 @@ func _physics_process(delta : float):
 	_player_run(delta)
 	_player_jump(delta)
 	_muzzle_position()
-	_run_shoot(delta)
-	_stand_shoot(delta)
-	
+	player_shooting(delta)
 	move_and_slide()
 	
 	_player_animations(delta)
@@ -53,19 +51,16 @@ func _player_idle(delta : float):
 		current_state = State.Idle
 
 
-func _run_shoot(delta):
-	var direction = _input_movement()
-	print("direction:",direction)
-	if direction != 0 and Input.is_action_just_pressed("shoot"):
-		_shoot_bullet()
-		current_state = State.RunShoot
 
-func _shoot_bullet():
+func player_shooting(delta):
 	var direction = _input_movement()
-	var bullet_instance = bullet.instantiate() as Node2D
-	bullet_instance.direction = direction
-	bullet_instance.global_position = muzzle.global_position
-	get_parent().add_child(bullet_instance)
+	if direction != 0 and Input.is_action_just_pressed("shoot"):
+		var bullet_instance = bullet.instantiate() as Node2D
+		print("direction:", direction)
+		bullet_instance.direction = direction
+		bullet_instance.global_position = muzzle.global_position
+		get_parent().add_child(bullet_instance)
+		current_state = State.RunShoot
 
 
 func _muzzle_position():
@@ -75,14 +70,6 @@ func _muzzle_position():
 	elif direction < 0:
 		muzzle.position.x = -muzzle_position.x
 
-func _stand_shoot(delta):
-	var direction = _input_movement()
-	var shoot_direction = _shoot_direction()
-	print("shoot_direction:", shoot_direction)
-	if direction == 0 and Input.is_action_just_pressed("shoot"):
-		_shoot_bullet()
-		current_state = State.StandShoot
-		print("stand-shoot")
 	
 func _player_run(delta : float):
 	var direction = _input_movement()
@@ -125,13 +112,5 @@ func _player_animations(delta : float):
 
 func _input_movement():
 	var direction : float = Input.get_axis("move_left", "move_right")
+	
 	return direction
-
-func _shoot_direction():
-	var direction = _input_movement()
-	var shoot_direction : float = 1
-	if direction != 0:
-		shoot_direction = direction
-	if shoot_direction == 0 and shoot_direction != -1:
-		shoot_direction = 1
-	return shoot_direction
